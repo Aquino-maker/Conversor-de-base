faa3e0d1086e4eec9bf7d4a59f00be100965c1c8
Aquino001 <luan.a.souza10@aluno.senai.br>
2025-01-03 18:32:38 -0300

    Adição da tela do programa!

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ co3 = "#38576b"  # valor
 co4 = "#403d3d"  # letra
 co5 = '#e89613'  # laranja
 
+janela = Tk()
+janela.title('Calculadora de conversão')
+janela.geometry('400x310')
+janela.configure(bg=co1)
+
+style = ttk.Style()
+style.theme_use('clam')
+ttk.Separator(janela, orient=HORIZONTAL).grid(row=0, columnspan=1, ipadx=190)
+
+janela.mainloop()
