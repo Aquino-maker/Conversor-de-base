6a1e5b85fe18578acd508123ef6810db88f4e5c8
Aquino001 <luan.a.souza10@aluno.senai.br>
2025-01-03 18:22:18 -0300

    Começando o projeto, adicionado cores e importando biblioteca TKINTER

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+# Cores
+co0 = "#444466"  # Preta
+co1 = "#feffff"  # branca / white
+co2 = "#6f9fbd"  # azul
+co3 = "#38576b"  # valor
+co4 = "#403d3d"  # letra
+co5 = '#e89613'  # laranja
+
